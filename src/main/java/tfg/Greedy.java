6f1f239afbd4a91242bf92f4aa88d212bd91939f
Luis Abril Andreu <Luisabrilandreu@gmail.com>
2026-01-30 11:38:20 +0100

    little change for upload branch

--- src/main/java/tfg/Greedy.java
+++ src/main/java/tfg/Greedy.java
@@ -149,7 +149,7 @@ public class Greedy {
         
         // Calidad = distancia + producción (normalizado)
         // Menor distancia y menor producción = mejor calidad (menor valor)
-        double quality = distance + node.getProd()/100; // Normalizar producción
+        double quality = distance + node.getProd() / 100; // Normalizar producción
         
         return quality;
     }
